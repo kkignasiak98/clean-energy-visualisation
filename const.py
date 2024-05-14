ba6e6f17fe77196ef29ca0235ab416fcf00d0f06
kkignasiak.98 <kkignasiak.98@gmail.com>
2024-05-14 18:07:17 +0000

    add `const.py`

---
+++ const.py
@@ -0,0 +1,67 @@
+# Geographic constans
+continents_names = ("World", 
+                    "Europe",
+                    "Africa",
+                    "Asia Pacific",
+                    "Australia",
+                    "China",
+                    "India",
+                    "Middle East",
+                    "North America",
+                    "South America")
+country_of_europe_names = ("World", 
+                            "Europe",
+                            "Austria",
+                            "Belarus",
+                            "Belgium",
+                            "Czechia",
+                            "Denmark",
+                            "Estonia",
+                            "Finland",
+                            "France",
+                            "Greece",
+                            "Hungary",
+                            "Iceland",
+                            "Ireland",
+                            "Italy",
+                            "Latvia",
+                            "Lithuania",
+                            "United Kingdom",
+                            "Ukraine",
+                            "Switzerland",
+                            "Sweden",
+                            "Portugal",
+                            "Poland",
+                            "Norway",
+                            "Luxembourg",
+                            "Bulgaria",
+                            "Germany",
+                            "Denmark",
+                            "Spain",
+                            "Albania",
+                            "Bosnia and Herzegovina",
+                            "Croatia",
+                            "Cyprus",
+                            "Kosovo",
+                            "Malta",
+                            "Moldova",
+                            "Netherlands",
+                            "Romania",
+                            "Russia",
+                            "Serbia",
+                            "Slovakia")
+
+# Visualisation
+
+plot_font_dict = dict(
+    family="Arial",
+    size=20,
+    color='#000000'
+)
+plot_height = 800
+plot_width= 1400
+
+pastel_main = px.colors.qualitative.Pastel
+sequential = px.colors.sequential.algae
+colors_for_pie =['mediumturquoise', 'gold', 'darkorange', 'lightgreen']
+colour_dict_for_pie = dict(colors=colors_for_pie, line=dict(color='#000000', width=2))
